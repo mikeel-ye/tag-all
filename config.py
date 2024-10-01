51b3bdcc6886ee3d018d5ad3bdb454acc7a96f51
Dareen <146856328+mikeel-ye@users.noreply.github.com>
2024-10-02 00:09:26 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 
-API_ID = int(os.environ.get("APP_ID", "22016511"))
+API_ID = int(os.environ.get("API_ID", "22016511"))
 API_HASH = os.environ.get("API_HASH", "8267335b1cbd700a179dbe9a9a482d1e")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7227138679:AAEaCRmeT_AjqvXuGEcHpRg5nC1COFraWHI")
